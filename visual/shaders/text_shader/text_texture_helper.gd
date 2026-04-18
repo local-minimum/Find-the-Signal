68cb3f0f48fba127345436021a19dc6961c17b31
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-04-18 09:54:28 +0200

    Add text shader

---
+++ visual/shaders/text_shader/text_texture_helper.gd
@@ -0,0 +1,63 @@
+@tool
+extends Node
+
+const SPACE: int = -1
+const MAX_TEXT_LENGTH: int = 32
+
+@export var mesh: MeshInstance3D:
+    set(value):
+        mesh = value
+        _sync()
+
+@export var override_mat_idx: int = 0:
+    set(value):
+        override_mat_idx = value
+        _sync()
+
+@export var alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;:?!\"'+-=*%_()[]{}~#&@©®™°^`|/\\<>…€$£¢¿¡“”‘’«»‹›„‚·•ÀÁÂÄÃÅĄÆÇĆÐÈÉÊËĘĞÌÍÎÏİŁÑŃÒÓÔÖÕŐØŒŚŞẞÞÙÚÛÜŰÝŸŹŻàáâäãåąæçćðèéêëęğìíîïıłñńòóôöõőøœśşßþùúûüűýÿźżАБВГҐДЕЁЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгґдеёєжзиіїйклмнопрстуфхцчшщъыьэюя":
+    set(value):
+        alphabet = value
+        _sync()
+
+@export var text: String:
+    set(value):
+        text = value
+        _sync()
+
+@export var unsupported_character: String = " "
+
+func _sync() -> void:
+    if mesh == null:
+        return
+
+    var mat: ShaderMaterial = mesh.get_surface_override_material(override_mat_idx)
+    if mat == null:
+        push_warning("%s of %s isn't a shader material" % [mesh.get_surface_override_material(override_mat_idx), mesh])
+        return
+
+    var packed: PackedInt32Array = PackedInt32Array()
+
+    # Add space for end of word when needed
+    var length = mini(text.length(), MAX_TEXT_LENGTH)
+    packed.resize(length)
+
+    var idx: int = 0
+    for ch: String in text:
+        if ch == " ":
+            packed[idx] = SPACE
+
+        var id: int = alphabet.find(ch)
+        if id < 0:
+            push_warning("'%s' not in alphabet" % [ch])
+
+            id = alphabet.find(unsupported_character[0])
+
+        packed[idx] = id
+        idx += 1
+
+        if idx >= MAX_TEXT_LENGTH:
+            break
+
+    mat.set_shader_parameter("word", packed)
+    mat.set_shader_parameter("word_length", length)
+    #print_debug("Setting %s to '%s' (%s)" % [mesh, text, packed])
