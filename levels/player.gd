c39aae4568571327f18bfd16d2a7c3837c903f6a
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-04-18 13:16:21 +0200

    Extend player to support controllers

--- levels/player.gd
+++ levels/player.gd
@@ -1,25 +1,48 @@
 extends CharacterBody3D
 
 @export var cam: Camera3D
+@export var invert_y: bool
 
 const SPEED: float = 2.0
-const JUMP_VELOCITY: float = 4.5
-const TURN_SPEED: float = PI * 0.001
-const LOOK_SPEED: float = PI * 0.001
+const JUMP_VELOCITY: float = 1.5
+
+const MOUSE_TURN_SPEED: float = PI * 0.001
+const MOUSE_LOOK_SPEED: float = PI * 0.001
+const JOY_TURN_SPEED: float = PI * 0.75
+const JOY_LOOK_SPEED: float = PI * 0.5
+
 const MOUSE_DEADZONE: float = 0.001
 const MAX_PITCH_CAM: float = PI * 0.3
 
 func _ready() -> void:
     Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+var _joy_look: Vector2
+var _using_joy_look: bool
+
 func _input(event: InputEvent) -> void:
     if event is InputEventMouseMotion:
         var m_event: InputEventMouseMotion = event
-        var rel: Vector2 = m_event.relative
+        _process_rel_look(Vector2(-m_event.relative.x * MOUSE_TURN_SPEED, m_event.relative.y * MOUSE_LOOK_SPEED))
+        _using_joy_look = false
+
+    elif event is InputEventJoypadMotion:
+        var rel: Vector2 = Input.get_vector("player_look_right", "player_look_left", "player_look_up", "player_look_down")
+        _joy_look = Vector2(rel.x * JOY_TURN_SPEED, rel.y * JOY_LOOK_SPEED)
+        _using_joy_look = true
+
+    if Input.is_action_just_pressed("pause"):
+        get_tree().quit()
+
+
+func _process_rel_look(rel: Vector2) -> void:
         if abs(rel.x) > MOUSE_DEADZONE:
-            rotate(up_direction, -rel.x * TURN_SPEED)
+            rotate(up_direction, rel.x)
         if abs(rel.y) > MOUSE_DEADZONE:
-            cam.rotation = (cam.rotation + rel.y * Vector3.LEFT * LOOK_SPEED).clampf(-MAX_PITCH_CAM, MAX_PITCH_CAM)
+            if invert_y:
+                cam.rotation = (cam.rotation - rel.y * Vector3.LEFT).clampf(-MAX_PITCH_CAM, MAX_PITCH_CAM)
+            else:
+                cam.rotation = (cam.rotation + rel.y * Vector3.LEFT).clampf(-MAX_PITCH_CAM, MAX_PITCH_CAM)
 
 func _physics_process(delta: float) -> void:
     # Add the gravity.
@@ -29,6 +52,9 @@ func _physics_process(delta: float) -> void:
     # Handle jump.
     if Input.is_action_just_pressed("player_jump") and is_on_floor():
         velocity.y = JUMP_VELOCITY
+
+    if _using_joy_look && _joy_look != Vector2.ZERO:
+        _process_rel_look(_joy_look * delta)
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
